faa2f4a954085b28b633d65b70042220f4722743
Donald Schulzm <donald@thewebweweave.com>
2021-02-25 19:03:06 -0800

    started putting together the code to call the api

--- src/version.python/version.py
+++ src/version.python/version.py
@@ -1,6 +1,9 @@
 import pygit2
 import requests
 
+key="d4756207-07ac-4ffa-beb0-3149ebe0fe19"
+product="semverservicesample"
+
 def getTip(repo):
     result = ""
     head = repo.head
@@ -10,11 +13,18 @@ def getTip(repo):
     result = parts[2]+'/'+parts[3]
     return result
 
+def sendToServer(branch):
+    url = 'https://localhost:5001/api/Version/'+ key
+    payload = {'Product': product, 'Branch': branch}
+    r = requests.get(url, params=payload)
+    print(r.url)
+
 def main():
     repo = pygit2.Repository('/home/ubuntu/repo/semverservicesample/.git')
     branch = getTip(repo)
 
     print(branch)
+    sendToServer(branch)
 
 if __name__ == "__main__":
     main()
