60060db785b46ff496d53b5dbe8b33caca545f03
Donald Schulz <donald@thewebweweave.com>
2021-03-18 17:36:11 -0700

    the publish file had a wrong file name to publish

--- build/ci/publish.sh
+++ build/ci/publish.sh
@@ -1,3 +1,3 @@
 cat /var/jenkins_home/my_password.txt | docker login -u schulzdl --password-stdin
 docker tag semverservice schulzdl/semverservice:$1
-docker push schulzdl/semverservice.api:$1
+docker push schulzdl/semverservice:$1
